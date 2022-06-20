3236c700ef5128a2c43be503f4e616933227ed58
vitkarpov <viktor.s.karpov@gmail.com>
2022-06-20 22:10:12 +0100

    Tests should pass

--- backend/db/comments.go
+++ backend/db/comments.go
@@ -8,17 +8,17 @@ import (
 
 func (db Database) GetComments() *models.GetComments {
 	response := &models.GetComments{}
-	_, err := db.Conn.Query("SELECT * FROM comments ORDER BY created_at DESC")
+	rows, err := db.Conn.Query("SELECT * FROM comments ORDER BY created_at DESC")
 	if err != nil {
 		log.Fatalf("Scan: %v", err)
 	}
-	// for rows.Next() {
-	// 	var comment models.Comment
-	// 	err := rows.Scan(&comment.ID, &comment.ParentID, &comment.AuthorName, &comment.Comment, &comment.CreatedAt)
-	// 	if err != nil {
-	// 		log.Fatalf("Scan: %v", err)
-	// 	}
-	// 	response.Comments = append(response.Comments, comment)
-	// }
+	for rows.Next() {
+		var comment models.Comment
+		err := rows.Scan(&comment.ID, &comment.ParentID, &comment.AuthorName, &comment.Comment, &comment.CreatedAt)
+		if err != nil {
+			log.Fatalf("Scan: %v", err)
+		}
+		response.Comments = append(response.Comments, comment)
+	}
 	return response
 }
